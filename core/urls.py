8bd74c9a7e15ce749b07778eb0bbbe887a56d283
Daniel An <118602647+danielan987@users.noreply.github.com>
2025-09-20 13:58:55 -0400

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,1 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("increment/", views.increment_counter, name="increment"),
+]
 
